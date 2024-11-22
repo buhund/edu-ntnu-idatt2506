56393ce3792d63ddaacf5223999a92cbe8dcb7d1
Eriksen <8539609+buhund@users.noreply.github.com>
2024-11-22 08:37:58 +0100

    Initial commit

--- assignment_08_project-FUCK/app/src/main/java/edu/ntnu/assignment_08_project/data/models/ListData.kt
+++
@@ -1,7 +0,0 @@
-package edu.ntnu.assignment_08_project.data.models
-
-data class ListData(
-    val listName: String,
-    val listItems: MutableList<ListItem>
-)
-
--- assignment_08_project-FUCK/app/src/main/java/edu/ntnu/assignment_08_project/data/models/ListItem.kt
+++
@@ -1,6 +0,0 @@
-package edu.ntnu.assignment_08_project.data.models
-
-data class ListItem(
-    val name: String,
-    var isDone: Boolean
-)
